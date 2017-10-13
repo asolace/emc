baf07299d0bd042a5c44d187f07daff71af4a60f
Jack H <jackhe678@gmail.com>
2017-10-13 15:33:25 -0400

    change comming soon image size

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-      <div className="about-page"><img src="http://chemvoice.com/wp-content/uploads/2016/11/comingsoon.png" alt="comming-soon" /></div>
+      <div className="about-page"><img className="comming-soon-img" src="http://chemvoice.com/wp-content/uploads/2016/11/comingsoon.png" alt="comming-soon" /></div>
     )
   }
 }
