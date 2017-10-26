147acc9afc6da5b72d8483077343e211c511410d
sangbpark94 <sangbp1994@gmail.com>
2017-10-26 16:46:14 -0400

    changed the mission section code to try and minimize the css and commented out all the @media screens for about page

--- src/components/About.js
+++ src/components/About.js
@@ -8,13 +8,20 @@ class About extends Component {
   render() {
     return (
       <div className="about-page-container">
-        <div className="about-mission-statement-out-container">
-          <h1 className="about-our-mission">Our Mission</h1>
-          <div className="about-mission-statement-in-container">
-            <ScrollAnimation animateIn='fadeIn' initiallyVisible={false} animateOnce={true}><p className="about-mission-statement">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac habitasse platea dictumst. Aliquam ut accumsan mauris. Duis et posuere magna. Fusce sed lacinia purus. Suspendisse tincidunt lobortis diam in efficitur. Praesent malesuada nisi pretium ante elementum, egestas vestibulum massa imperdiet. Nulla id sodales erat.
-            </p></ScrollAnimation>
-          </div>
+        <div className="about-mission-container">
+          <h1 className="about-mission-header">Our Mission</h1>
+          <ScrollAnimation animateIn='fadeIn' initiallyVisible={false} animateOnce={true}>
+            <p className="about-mission-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec nec dui et enim commodo facilisis id in magna.
+            Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu.
+            Aenean et diam egestas, vestibulum orci sodales, tristique odio.
+            Aliquam quis varius quam, ultrices euismod ipsum.
+            Integer gravida iaculis mauris ac tristique.
+            </p>
+          </ScrollAnimation>
         </div>
+
         <br /><br /><br /><br />
         <hr />
 
@@ -23,19 +30,19 @@ class About extends Component {
           <div className="about-container">
             <Row className="about-container-row">
               <Col><div className="about-leader-image-container"><img className="about-leader-image" src={profilepic}></img></div></Col>
-              <Col><div className="about-leader-text-container"><h2>Pastor Jae Lee</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst.</div></Col>
+              <Col><div className="about-leader-text"><h2>Pastor Jae Lee</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst.</div></Col>
             </Row>
             <Row className="about-container-row">
               <Col><div className="about-leader-image-container"><img className="about-leader-image" src={profilepic}></img></div></Col>
-              <Col><div className="about-leader-text-container"><h2>Pastor Pio Lee</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst.</div></Col>
+              <Col><div className="about-leader-text"><h2>Pastor Pio Lee</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst.</div></Col>
             </Row>
             <Row className="about-container-row">
               <Col><div className="about-leader-image-container"><img className="about-leader-image" src={profilepic}></img></div></Col>
-              <Col><div className="about-leader-text-container"><h2>Pastor Enrique Pinero</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst.</div></Col>
+              <Col><div className="about-leader-text"><h2>Pastor Enrique Pinero</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst.</div></Col>
             </Row>
             <Row className="about-container-row">
               <Col><div className="about-leader-image-container"><img className="about-leader-image" src={profilepic}></img></div></Col>
-              <Col><div className="about-leader-text-container"><h2>Dr. Soo-Il Lee</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst.</div></Col>
+              <Col><div className="about-leader-text"><h2>Dr. Soo-Il Lee</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst.</div></Col>
             </Row>
           </div>
         </ScrollAnimation>
