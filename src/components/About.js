b96d6d65b5d5e023a973ece1ebae576bd2e225c3
sangbpark94 <sangbp1994@gmail.com>
2017-10-26 12:44:04 -0400

    added animations to about page using react-animate-on-scroll library

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import grouppic from '../photos/people/IMG_0582.JPG'
-import profilepic from '../photos/tempprofilepic.jpg'
+import grouppic from '../photos/people/IMG_0582.JPG';
+import profilepic from '../photos/tempprofilepic.jpg';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 class About extends Component {
   render() {
@@ -10,60 +11,64 @@ class About extends Component {
         <div className="about-mission-statement-out-container">
           <h1 className="about-our-mission">Our Mission</h1>
           <div className="about-mission-statement-in-container">
-            <p className="about-mission-statement">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac habitasse platea dictumst. Aliquam ut accumsan mauris. Duis et posuere magna. Fusce sed lacinia purus. Suspendisse tincidunt lobortis diam in efficitur. Praesent malesuada nisi pretium ante elementum, egestas vestibulum massa imperdiet. Nulla id sodales erat.
-            </p>
+            <ScrollAnimation animateIn='fadeIn' initiallyVisible={false} animateOnce={true}><p className="about-mission-statement">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac habitasse platea dictumst. Aliquam ut accumsan mauris. Duis et posuere magna. Fusce sed lacinia purus. Suspendisse tincidunt lobortis diam in efficitur. Praesent malesuada nisi pretium ante elementum, egestas vestibulum massa imperdiet. Nulla id sodales erat.
+            </p></ScrollAnimation>
           </div>
         </div>
         <br /><br /><br /><br />
-        <hr className="about-hr"/>
+        <hr />
 
         <h1 className="about-our-leaders">Our Leaders</h1>
-        <div className="about-container">
-          <Row className="about-container-row">
-            <Col><div className="about-leader-image-container"><img className="about-leader-image" src={profilepic}></img></div></Col>
-            <Col><div className="about-leader-text"><h2 className="about-leader-text-header">Pastor Jae Lee</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst. </div></Col>
-          </Row>
-          <Row className="about-container-row">
-            <Col><div className="about-leader-image-container"><img className="about-leader-image" src={profilepic}></img></div></Col>
-            <Col><div className="about-leader-text"><h2 className="about-leader-text-header">Pastor Pio Lee</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst. </div></Col>
-          </Row>
-          <Row className="about-container-row">
-            <Col><div className="about-leader-image-container"><img className="about-leader-image" src={profilepic}></img></div></Col>
-            <Col><div className="about-leader-text"><h2 className="about-leader-text-header">Pastor Enrique Pinero</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst. </div></Col>
-          </Row>
-          <Row className="about-container-row">
-            <Col><div className="about-leader-image-container"><img className="about-leader-image" src={profilepic}></img></div></Col>
-            <Col><div className="about-leader-text"><h2 className="about-leader-text-header">Dr. Soo-Il Lee</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst. </div></Col>
-          </Row>
-        </div>
+        <ScrollAnimation animateIn='fadeIn' initiallyVisible={false} animateOnce={true}>
+          <div className="about-container">
+            <Row className="about-container-row">
+              <Col><div className="about-leader-image-container"><img className="about-leader-image" src={profilepic}></img></div></Col>
+              <Col><div className="about-leader-text-container"><h2>Pastor Jae Lee</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst.</div></Col>
+            </Row>
+            <Row className="about-container-row">
+              <Col><div className="about-leader-image-container"><img className="about-leader-image" src={profilepic}></img></div></Col>
+              <Col><div className="about-leader-text-container"><h2>Pastor Pio Lee</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst.</div></Col>
+            </Row>
+            <Row className="about-container-row">
+              <Col><div className="about-leader-image-container"><img className="about-leader-image" src={profilepic}></img></div></Col>
+              <Col><div className="about-leader-text-container"><h2>Pastor Enrique Pinero</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst.</div></Col>
+            </Row>
+            <Row className="about-container-row">
+              <Col><div className="about-leader-image-container"><img className="about-leader-image" src={profilepic}></img></div></Col>
+              <Col><div className="about-leader-text-container"><h2>Dr. Soo-Il Lee</h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dui et enim commodo facilisis id in magna. Nam ullamcorper nulla nunc, quis maximus lacus consectetur eu. Aenean et diam egestas, vestibulum orci sodales, tristique odio. Aliquam quis varius quam, ultrices euismod ipsum. Integer gravida iaculis mauris ac tristique. Donec semper, mauris eget hendrerit ornare, sapien quam gravida odio, a tincidunt purus ante in ligula. In hac  habitasse platea dictumst.</div></Col>
+            </Row>
+          </div>
+        </ScrollAnimation>
 
         <hr />
 
         <h1 className="about-our-leaders">Our Deacons</h1>
-
-        <div className="about-deacon-container">
-          <Row className="about-deacon-row">
-            <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Lulu Lim</h2></Col>
-            <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Mimi Ahn</h2></Col>
-            <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Kay Choi</h2></Col>
-            <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Arlene Macaraeg</h2></Col>
-          </Row>
-          <Row className="about-deacon-row">
-            <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Nilsa Morales</h2></Col>
-            <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Jason Park</h2></Col>
-            <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Daniel Hsin</h2></Col>
-            <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Virgil Macaraeg</h2></Col>
-          </Row>
-        </div>
+        <ScrollAnimation animateIn='fadeIn' initiallyVisible={false} animateOnce={true}>
+          <div className="about-deacon-container">
+            <Row className="about-deacon-row">
+              <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Lulu Lim</h2></Col>
+                <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Mimi Ahn</h2></Col>
+              <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Kay Choi</h2></Col>
+              <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Arlene Macaraeg</h2></Col>
+            </Row>
+            <Row className="about-deacon-row">
+              <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Nilsa Morales</h2></Col>
+              <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Jason Park</h2></Col>
+              <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Daniel Hsin</h2></Col>
+              <Col><div className="about-deacon-image-container"><img className="about-deacon-image" src={profilepic}></img></div><h2 className="about-deacon-name">Virgil Macaraeg</h2></Col>
+            </Row>
+          </div>
+        </ScrollAnimation>
 
         <hr />
 
         <h1 className="about-restofus-header">And the rest of our family!</h1>
 
-        <div className="about-restofus-frame">
-          <img className="about-restofus-image" src={grouppic}></img>
-        </div>
-
+        <ScrollAnimation animateIn='fadeIn' initiallyVisible={false} animateOnce={true}>
+          <div className="about-restofus-frame">
+            <img className="about-restofus-image" src={grouppic}></img>
+          </div>
+        </ScrollAnimation>
         <br/><br/><br/><br/>
 
       </div>
